1fc36617a55cb39a2c9b6b7b8cab337b5084d708
Aodj05 <aodj05@msn.com>
2022-08-22 15:31:24 -0400

    StartMenu

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import { StartMenu } from "components/StartMenu";
+import { useState } from "react";
+import styles from "./styles.module.css";
+
+export const App = () => {
+  const [mode, setMode] = useState("start");
+
+  return (
+    <div className={styles.main}>
+      {mode === "start" && (
+        <StartMenu onStartClick={() => setMode("battle")} />
+      )}
+
+      {mode === "battle" && <>Battle mode</>}
+
+      {mode === "gameOver" && <>Game Over</>}
+    </div>
+  );
+};
