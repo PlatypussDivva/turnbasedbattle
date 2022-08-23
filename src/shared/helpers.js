de371cb60ccdcd3b54a2e3d4539f319a924ddaa7
Aodj05 <aodj05@msn.com>
2022-08-23 00:27:54 -0400

    Update helpers.js

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -4,3 +4,25 @@ export const wait = ms =>
       resolve();
     }, ms);
   });
+
+export const attack = ({ attacker, defender }) => {
+  const receivedDamage =
+    attacker.attack - (attacker.level - defender.level) * 1.25;
+
+  const finalDamage = receivedDamage - defender.defense / 2;
+
+  return finalDamage;
+};
+
+export const magic = ({ attacker, defender }) => {
+  const receivedDamage =
+    attacker.magic - (attacker.level - defender.level) * 1.25;
+
+  const finalDamage = receivedDamage - defender.magicDefense / 2;
+
+  return finalDamage;
+};
+
+export const heal = ({ defender }) => {
+  return defender.magic + defender.level * 0.25;
+};
